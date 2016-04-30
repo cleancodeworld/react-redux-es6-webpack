7b66fb7d76eb65cef57966815af2bdf0edeb26c9
cleancodeworld <killianlake2019@gmail.com>
2016-04-30 14:09:19 +0300

    load limitless theme

--- src/theme/bootstrap.config.prod.js
+++ src/theme/bootstrap.config.prod.js
@@ -1,5 +1,5 @@
 const bootstrapConfig = require('./bootstrap.config.js');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-bootstrapConfig.styleLoader = ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader');
+bootstrapConfig.styleLoader = ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader');
 module.exports = bootstrapConfig;
 
