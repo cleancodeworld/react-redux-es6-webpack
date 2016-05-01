4d3802bc0ded3dde5c489c5b46d7f7d56c942ba4
cleancodeworld <killianlake2019@gmail.com>
2016-05-01 12:35:27 +0300

    Remove useless containers

--- src/routes/author.js
+++ src/routes/author.js
@@ -1,37 +1,21 @@
 import React from 'react';
 import {IndexRoute, Route} from 'react-router';
 import {
-    App,
-    Home,
-    NotFound,
-    LoginSuccess, // temporary
-  } from '../containers';
-import {
-    Portal
-  } from '../containers/Author';
+  App,
+  Portal
+} from '../containers/portal';
 
-export default (funcs) => {
-  // const {requireLogin, subdomain} = funcs;
-  const {requireLogin} = funcs;
-
+export default () => {
   /**
    * Please keep routes in alphabetical order
    */
   return (
     <Route path="/" component={App}>
       { /* Home (main) route */ }
-      <IndexRoute component={Home}/>
+      <IndexRoute component={Portal}/>
 
       { /* Sample component for dynamic subdomain routing */ }
-      <Route path="portal/:portalname" component={Portal} />
-
-      { /* Routes requiring login */ }
-      <Route onEnter={requireLogin}>
-        <Route path="loginSuccess" component={LoginSuccess}/>
-      </Route>
-
-      { /* Catch all route */ }
-      <Route path="*" component={NotFound} status={404} />
+      <Route path="portal/:portalname" component={Portal}/>
     </Route>
   );
 };
--- src/routes/main.js
+++ src/routes/main.js
@@ -2,19 +2,12 @@ import React from 'react';
 import {IndexRoute, Route} from 'react-router';
 import {
     App,
-    Chat,
     Home,
-    Widgets,
-    About,
     Login,
-    LoginSuccess,
-    Survey,
     NotFound,
-  } from '../containers';
+  } from '../containers/bare';
 
-export default (funcs) => {
-  const {requireLogin} = funcs;
-
+export default () => {
   /**
    * Please keep routes in alphabetical order
    */
@@ -23,17 +16,8 @@ export default (funcs) => {
       { /* Home (main) route */ }
       <IndexRoute component={Home}/>
 
-      { /* Routes requiring login */ }
-      <Route onEnter={requireLogin}>
-        <Route path="chat" component={Chat}/>
-        <Route path="loginSuccess" component={LoginSuccess}/>
-      </Route>
-
       { /* Routes */ }
-      <Route path="about" component={About}/>
       <Route path="login" component={Login}/>
-      <Route path="survey" component={Survey}/>
-      <Route path="widgets" component={Widgets}/>
 
       { /* Catch all route */ }
       <Route path="*" component={NotFound} status={404} />
