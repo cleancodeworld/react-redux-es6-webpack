f770e3d50bb8c33ba6e65dffd8ab058585f9b010
cleancodeworld <killianlake2019@gmail.com>
2016-05-08 23:36:06 +0300

    improvements

--- webpack/utils/last-commit.js
+++ webpack/utils/last-commit.js
@@ -1,7 +1,7 @@
 var git = require('git-rev-sync')
 
 function getLastCommit() {
-  var lastCommit = '"' + git.short().toString() + '"';
+  var lastCommit = '\'' + git.short().toString() + '\'';
   return lastCommit.replace(/"/g, '');
 }
 
