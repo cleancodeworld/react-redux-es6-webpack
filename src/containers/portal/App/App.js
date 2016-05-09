0ec07c8746e75f82f1b1e1ad2464e569be69e7a7
cleancodeworld <killianlake2019@gmail.com>
2016-05-10 01:43:48 +0300

    KX-128

--- src/containers/portal/App/App.js
+++ src/containers/portal/App/App.js
@@ -1,17 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import { asyncConnect } from 'redux-async-connect';
-import { isChecked, check as portalCheck } from 'redux/modules/portal';
 import { load } from 'redux/modules/course/edit';
 
 @asyncConnect([{
   promise: ({store: {dispatch, getState}, params}) => {
     const promises = [];
     const state = getState();
-    if (!isChecked(state) || true) {
-      if (state.portal && state.portal.get('reqSubdomain')) {
-        promises.push(dispatch(portalCheck(state.portal.get('reqSubdomain'))));
-      }
-    }
     if (params.courseName !== '') {
       promises.push(dispatch(load(params.courseName)));
     }
