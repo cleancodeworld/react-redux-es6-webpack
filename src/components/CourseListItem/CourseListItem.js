8d7c8045566e61e2a692d3a7e5bbab5988197182
cleancodeworld <killianlake2019@gmail.com>
2016-05-10 14:58:23 +0300

    Remove useless html from CourseListItem

--- src/components/CourseListItem/CourseListItem.js
+++ src/components/CourseListItem/CourseListItem.js
@@ -11,31 +11,14 @@ const CourseListItem = ({course, author}) => {
         <div className="caption">
           <h6 className="no-margin-top text-semibold"><Link to={'/author/course/' + course.get('Slug')} className="text-default">{course.get('name')}</Link></h6>
           <hr className="no-margin-top mb-10"/>
-          <div className="clearfix">
-            <div className="pull-left">
-              Price <b className="clearfix">$35</b>
-            </div>
-            <div className="pull-right">
-              <b>216</b> Reviews
-              <div className="star">
-                <i className="icon-star-full2 text-size-base text-warning-300"></i>
-                <i className="icon-star-full2 text-size-base text-warning-300"></i>
-                <i className="icon-star-full2 text-size-base text-warning-300"></i>
-                <i className="icon-star-full2 text-size-base text-warning-300"></i>
-                <i className="icon-star-half text-size-base text-warning-300"></i>
-              </div>
-            </div>
-          </div>
-          <hr className="no-margin-top mb-10 mt-10"/>
           <div className="media no-margin-top">
             <div className="media-left media-middle">
               <a href="#">
-                <img src="assets/images/placeholder.jpg" className="img-responsive" alt=""/>
+                <img src={'https://placehold.it/40x40'} className="img-responsive" alt=""/>
               </a>
             </div>
             <div className="media-body">
               <div className="media-heading text-semibold">{author.get('name')}</div>
-              <span className="text-muted ellipsis">Best-selling Instructor, Game </span>
             </div>
           </div>
         </div>
