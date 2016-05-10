a49d2294b83fe3ec682460a00464a0320570d2ff
cleancodeworld <killianlake2019@gmail.com>
2016-05-10 14:38:57 +0300

    remove useless links

--- src/components/PortalLayout/CourseRightMenu.js
+++ src/components/PortalLayout/CourseRightMenu.js
@@ -14,12 +14,7 @@ export default class CourseRightMenu extends Component {
         </div>
         <div className="list-group no-border no-padding-top">
           <Link to={'/author/course/' + courseName + '/goals'} className="list-group-item"><i className="icon-user"></i> Goals</Link>
-          <a href="#" className="list-group-item"><i className="icon-cash3"></i> Accounting</a>
           <Link to={'/author/course/' + courseName + '/lesson/list'} className="list-group-item"><i className="icon-tree7"></i> Curriculum <span className="badge bg-danger pull-right">2</span></Link>
-          <a href="#" className="list-group-item"><i className="icon-users"></i> SEO</a>
-          <div className="list-group-divider"></div>
-          <a href="#" className="list-group-item"><i className="icon-calendar3"></i> Co-Authors<span className="badge bg-teal-400 pull-right">48</span></a>
-          <a href="#" className="list-group-item"><i className="icon-cog3"></i> Metrics</a>
         </div>
       </div>
     );
