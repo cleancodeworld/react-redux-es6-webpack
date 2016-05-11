d2574b1ca60bdd78e339de6014d26a1181620242
cleancodeworld <killianlake2019@gmail.com>
2016-05-11 16:33:07 +0200

    Course images

--- src/components/CourseListItem/CourseListItem.js
+++ src/components/CourseListItem/CourseListItem.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {Link} from 'react-router';
 
 const CourseListItem = ({course, author}) => {
+  const courseImage = course.get('thumbnail');
+  const coursePlaceholderImage = '/assets/images/placeholder.jpg';
   return (
     <div className="col-lg-4 col-sm-6">
       <div className="thumbnail">
-        <div className="thumb">
-          <img src={course.get('thumbnail')} alt="Course Thumbnail"/>
+        <div className="thumb-wrapper" style={{ backgroundImage: 'url(' + coursePlaceholderImage + ')', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+          <div className="thumb" style={{ backgroundImage: 'url(' + courseImage + ')', backgroundSize: 'cover', backgroundPosition: 'center', paddingTop: '100%' }}>
+            <img src={courseImage} alt="Course Thumbnail" style={{ display: 'none' }}/>
+          </div>
         </div>
         <div className="caption">
           <h6 className="no-margin-top text-semibold"><Link to={'/author/course/' + course.get('slug')} className="text-default">{course.get('name')}</Link></h6>
