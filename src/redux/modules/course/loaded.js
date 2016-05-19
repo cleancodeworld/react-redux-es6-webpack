d55a571f781e2bc21e3f53a448b47ac162c7e9a8
cleancodeworld <killianlake2019@gmail.com>
2016-05-19 11:15:23 +0200

    Author info removed from courselist component

--- src/redux/modules/course/loaded.js
+++ src/redux/modules/course/loaded.js
@@ -46,11 +46,7 @@ import {
 } from 'utils/normalize';
 
 const initialState = Immutable.fromJS({
-  entities: {},
-  author: {
-    firstName: '',
-    lastName: ''
-  }
+  entities: {}
 });
 
 export default function courseLoad(state = initialState, action) {
