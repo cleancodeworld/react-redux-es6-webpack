e3aaaac587e078bb98b1720e637e799184cab79a
cleancodeworld <killianlake2019@gmail.com>
2016-05-24 17:22:54 +0200

    Course create fix, portal create module relocate

--- src/redux/modules/portal/create.js
+++ src/redux/modules/portal/create.js
@@ -22,7 +22,7 @@ export function createPortal(model, ownerId) {
   return dispatch => {
     return dispatch(create(model))
       .then((res)=> {
-        window.location.href = `${location.protocol}//${res.createdPortal.name}.${config.mainDomain}`;
+        window.location.href = `${location.protocol}//${res.data.portal.name}.${config.mainDomain}`;
       })
       .catch(res => {
         throw new SubmissionError({ _error: res.error });
