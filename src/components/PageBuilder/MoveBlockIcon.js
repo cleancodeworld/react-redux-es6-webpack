ab84dd08679c531dbf18b9cd2faf96fba539b769
cleancodeworld <killianlake2019@gmail.com>
2016-06-01 19:07:22 +0300

    init page builder

--- src/components/PageBuilder/MoveBlockIcon.js
+++
@@ -1,6 +0,0 @@
-import React, { Component, PropTypes } from 'react';
-export default class Card extends Component {
-  render() {
-    return <i style={{ ...style, opacity }} className="fa fa-sort"></i>;
-  }
-}
