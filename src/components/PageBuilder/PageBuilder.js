c4dd7a045347ba4a39fef0dffea134d3d4151318
cleancodeworld <killianlake2019@gmail.com>
2016-06-07 16:09:18 +0300

    linting

--- src/components/PageBuilder/PageBuilder.js
+++ src/components/PageBuilder/PageBuilder.js
@@ -1,3 +1,4 @@
+/* global $ */
 import React, { Component, PropTypes} from 'react';
 import TinyMCE from 'react-tinymce';
 import scriptLoader from 'react-async-script-loader';
@@ -23,24 +24,23 @@ export default class PageBuilder extends Component {
     if (!isScriptLoaded) return <div>Loading</div>;
     return (
       <div>
-        <iframe id="form_target" name="form_target" style={{display:'none'}}></iframe>
-        <form id="my_form" action="/upload/" target="form_target" method="post" enctype="multipart/form-data"
-              style={{width:'0',height:'0',overflow:'hidden'}}>
-          <input name="image" type="file"
-                 onchange="$('#my_form').submit();this.value='';"/>
+        <iframe id="form_target" name="form_target" style={{display: 'none'}}></iframe>
+        <form id="my_form" action="/upload/" target="form_target" method="post" encType="multipart/form-data"
+              style={{width: '0', height: '0', overflow: 'hidden'}}>
+          <input name="image" type="file" onChange="$('#my_form').submit();this.value='';"/>
         </form>
         <TinyMCE
           content="<p>This is the initial content of the editor</p>"
           config={{
-          plugins: 'autolink link image lists print preview media',
-          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright',
-          images_upload_url: 'postAcceptor.php',
-          images_upload_base_path: '/some/basepath',
-          images_upload_credentials: true,
-          file_browser_callback: function(field_name, url, type, win) {
-            if(type==='image') $('#my_form input').click();
-          }
-        }}
+            plugins: 'autolink link image lists print preview media',
+            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright',
+            images_upload_url: 'postAcceptor.php',
+            images_upload_base_path: '/some/basepath',
+            images_upload_credentials: true,
+            file_browser_callback: (fieldName, url, type) =>{
+              if (type === 'image') $('#my_form input').click();
+            }
+          }}
           onChange={this.handleEditorChange}
         />
       </div>
