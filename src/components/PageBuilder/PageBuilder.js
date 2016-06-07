e9f25aecf10dba112726de6272803244c2c3b935
cleancodeworld <killianlake2019@gmail.com>
2016-06-07 14:34:45 +0300

    init ability to upload images

--- src/components/PageBuilder/PageBuilder.js
+++ src/components/PageBuilder/PageBuilder.js
@@ -5,7 +5,8 @@ import {clientSideOnly} from 'hoc';
 
 @clientSideOnly
 @scriptLoader(
-  '//tinymce.cachefly.net/4.2/tinymce.min.js'
+  '//cdn.tinymce.com/4/tinymce.min.js',
+  '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'
 )
 export default class PageBuilder extends Component {
   static propTypes = {
@@ -21,14 +22,28 @@ export default class PageBuilder extends Component {
     const { isScriptLoaded } = this.props;
     if (!isScriptLoaded) return <div>Loading</div>;
     return (
-      <TinyMCE
-        content="<p>This is the initial content of the editor</p>"
-        config={{
+      <div>
+        <iframe id="form_target" name="form_target" style={{display:'none'}}></iframe>
+        <form id="my_form" action="/upload/" target="form_target" method="post" enctype="multipart/form-data"
+              style={{width:'0',height:'0',overflow:'hidden'}}>
+          <input name="image" type="file"
+                 onchange="$('#my_form').submit();this.value='';"/>
+        </form>
+        <TinyMCE
+          content="<p>This is the initial content of the editor</p>"
+          config={{
           plugins: 'autolink link image lists print preview media',
-          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright'
+          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright',
+          images_upload_url: 'postAcceptor.php',
+          images_upload_base_path: '/some/basepath',
+          images_upload_credentials: true,
+          file_browser_callback: function(field_name, url, type, win) {
+            if(type==='image') $('#my_form input').click();
+          }
         }}
-        onChange={this.handleEditorChange}
-      />
+          onChange={this.handleEditorChange}
+        />
+      </div>
     );
   }
 }
