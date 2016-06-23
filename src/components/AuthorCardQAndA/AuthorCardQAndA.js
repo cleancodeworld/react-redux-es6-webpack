d320da5dd09ceaa04876f9421aef3bbb3e78505b
cleancodeworld <killianlake2019@gmail.com>
2016-06-23 13:58:38 +0300

    disable user budgets

--- src/components/AuthorCardQAndA/AuthorCardQAndA.js
+++ src/components/AuthorCardQAndA/AuthorCardQAndA.js
@@ -6,7 +6,7 @@ const AuthorCardQAndA = ({author, createdAt})=> {
       <header className="mb-5">{moment(createdAt).fromNow()}</header>
       <img src={author.get('image')} width="40" height="40" alt="" className="mr-10"/>
       <a href="#">{author.get('username')}</a>
-      <div className="content-group no-margin">28 &nbsp;<span className="status-mark bg-danger"></span> 5</div>
+      <div className="content-group no-margin">[-] &nbsp;<span className="status-mark bg-danger"></span> [-]</div>
     </blockquote>
   );
 };
