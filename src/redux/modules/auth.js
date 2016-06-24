35484e7643cc0ce091d2fa477ba0d87e1e8ae74b
cleancodeworld <killianlake2019@gmail.com>
2016-06-24 18:55:11 +0300

    redirect to main domain on logout

--- src/redux/modules/auth.js
+++ src/redux/modules/auth.js
@@ -158,7 +158,8 @@ export function logout() {
       types: [LOGOUT, LOGOUT_SUCCESS, LOGOUT_FAIL],
       promise: (client) => client.post('/api/v1/logout')
     }).then(()=> {
-      return window.location.href = `${location.protocol}//${config.mainDomain}`;
+      console.log(`${location.protocol}//${config.mainDomain}`);
+      window.location.href = `${location.protocol}//${config.mainDomain}`;
     });
   };
 }
