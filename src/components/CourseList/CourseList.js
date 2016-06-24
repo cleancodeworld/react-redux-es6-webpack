012013b71a53382ccd4fe0dfd50ccece4dfc095b
cleancodeworld <killianlake2019@gmail.com>
2016-06-24 16:10:28 +0300

    Display `No courses yet` when there is no courses

--- src/components/CourseList/CourseList.js
+++ src/components/CourseList/CourseList.js
@@ -42,6 +42,14 @@ export default class CourseList extends Component {
           }
         </div>
       </div>
-    ) : <span>Empty</span>;
+    ) : (<div>
+      <div className="content-group tab-content-bordered navbar-component">
+        <div className="navbar navbar-default navbar-xs">
+          <div className="navbar-collapse collapse">
+            <div class="navbar-text h3">No courses yet</div>
+          </div>
+        </div>
+      </div>
+    </div> );
   }
 }
