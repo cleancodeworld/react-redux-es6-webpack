444368b324dd27a47ea00e17abddd4f13f37df1d
cleancodeworld <killianlake2019@gmail.com>
2016-06-26 15:48:49 +0300

    fix portal form validation

--- src/components/CreatePortalForm/validate.js
+++ src/components/CreatePortalForm/validate.js
@@ -1,8 +1,13 @@
+import isAlphanumeric from 'validator/lib/isAlphanumeric';
 const CreatePortalValidation = values => {
   const errors = {};
 
   if (!values.name) {
     errors.name = 'Required';
+  } else if (values.name.length < 100) {
+    errors.name = 'Name cannot be longer than 150 characters.';
+  } else if (!isAlphanumeric(values.name + '')) {
+    errors.name = 'Name should contain only letters and numbers';
   }
 
   if (!values.isAccepted) {
