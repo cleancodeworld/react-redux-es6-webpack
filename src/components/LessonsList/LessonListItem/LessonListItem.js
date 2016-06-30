14b4658fd53d3af61548361740d4823a1ff85e2a
cleancodeworld <killianlake2019@gmail.com>
2016-07-01 02:34:40 +0300

    issue #13 load lessons list

--- src/components/LessonsList/LessonListItem/LessonListItem.js
+++ src/components/LessonsList/LessonListItem/LessonListItem.js
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router';
-import {RemoveButton} from './RemoveButton/RemoveButton';
+import RemoveButton from './RemoveButton/RemoveButton';
 import moment from 'moment';
 
 export default class LessonListItem extends Component {
