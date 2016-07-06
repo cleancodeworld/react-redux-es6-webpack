7f1cf9dd3f880b641264277563c3fcec11e7b15e
cleancodeworld <killianlake2019@gmail.com>
2016-07-06 19:10:40 +0200

    Grammar fix

--- src/utils/errorBeautifier.js
+++ src/utils/errorBeautifier.js
@@ -3,7 +3,7 @@ import {SubmissionError} from 'redux-form';
 export default function beautifyAndThrow(errorMessage) {
   let error = '';
   if (/^Struct:/.test(errorMessage)) {
-    error = 'Validation failed for following fields: ';
+    error = 'Validation failed for the following fields: ';
     const failedFields = [];
     const validationErrors = errorMessage.match(/Field validation for "[a-zA-Z0-9]+" failed on the "[a-zA-Z]+" tag/g);
     validationErrors.forEach(validationError => {
