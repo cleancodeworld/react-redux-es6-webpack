94b57e9d485fe3728b4bc79295cbf7ae23eb9ffb
cleancodeworld <killianlake2019@gmail.com>
2016-07-10 18:37:54 +0300

    KX-224

--- src/components/LessonForm/validate.js
+++ src/components/LessonForm/validate.js
@@ -8,7 +8,7 @@ const LessonFormValidation = values => {
   } else if (values.title.length > 100) {
     errors.title = 'Title cannot be longer than 100 characters.';
   }
-  if (values.thumbnail && values.thumbnail.length) {
+  if (values.thumbnail && values.thumbnail.length && values.thumbnail[0].name) {
     const file = values.thumbnail[0].name.toLowerCase();
     if (file.indexOf('.jpg') === -1 && file.indexOf('.png') === -1 && file.indexOf('.jpeg') === -1 && file.indexOf('.gif') === -1) {
       errors.thumbnail = 'only images allowed';
