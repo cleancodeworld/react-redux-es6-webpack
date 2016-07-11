4cf5e1c9ae0e84031aaed2662dbf0af38aabfefb
cleancodeworld <killianlake2019@gmail.com>
2016-07-11 16:36:31 +0300

    KX-227

--- src/components/LessonForm/validate.js
+++ src/components/LessonForm/validate.js
@@ -7,7 +7,10 @@ const LessonFormValidation = values => {
     errors.title = 'Required';
   } else if (values.title.length > 100) {
     errors.title = 'Title cannot be longer than 100 characters.';
+  } else if (!/^[a-z0-9\s]+$/i.test(values.title)) {
+    errors.title = 'You can use only alphabets, numbers and spaces in title.';
   }
+
   if (values.thumbnail && values.thumbnail.length && values.thumbnail[0].name) {
     const file = values.thumbnail[0].name.toLowerCase();
     if (file.indexOf('.jpg') === -1 && file.indexOf('.png') === -1 && file.indexOf('.jpeg') === -1 && file.indexOf('.gif') === -1) {
