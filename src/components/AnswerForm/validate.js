ff686aab192eb3dafb67606061121a2d1caff42c
cleancodeworld <killianlake2019@gmail.com>
2016-07-17 10:55:18 +0300

    KX-220

--- src/components/AnswerForm/validate.js
+++ src/components/AnswerForm/validate.js
@@ -8,6 +8,8 @@ const validate = values => {
   const content = values.content && values.content.replace(/<(?:.|\n)*?>/gm, '') || '';
   if (!content) {
     errors.content = 'Body is missing';
+  } else if (content.length < 30) {
+    errors.content = 'Body can not be less than 30 characters.';
   } else if (content.length > 30000) {
     errors.content = 'Body can not be longer than 30000 characters.';
   }
--- src/components/QuestionForm/validate.js
+++ src/components/QuestionForm/validate.js
@@ -27,6 +27,8 @@ const validate = values => {
 
   if (!content) {
     errors.content = 'Body is missing';
+  } else if (content.length < 30) {
+    errors.content = 'Body can not be less than 30 characters.';
   } else if (content.length > 30000) {
     errors.content = 'Body can not be longer than 30000 characters.';
   }
