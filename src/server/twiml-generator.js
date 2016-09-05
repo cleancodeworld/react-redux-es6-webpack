fbea70713d3cdee885d53a19f0fdbfd605a06d4f
cleancodeworld <killianlake2019@gmail.com>
2016-09-05 12:20:11 +0300

    linting

--- src/server/twiml-generator.js
+++ src/server/twiml-generator.js
@@ -1,8 +1,6 @@
-'use strict';
+import twilio from 'twilio';
 
-var twilio = require('twilio');
-
-var connectConferenceTwiml = function(options) {
+export const connectConferenceTwiml = function(options) {
   return new twilio.TwimlResponse().dial(function(dialNode) {
     dialNode.conference(options.conferenceId, {
       'startConferenceOnEnter': options.startConferenceOnEnter,
@@ -13,11 +11,8 @@ var connectConferenceTwiml = function(options) {
   });
 };
 
-var waitResponseTwiml = function() {
+export const waitResponseTwiml = function() {
   return new twilio.TwimlResponse()
     .say('Thank you for calling. Please wait in line for a few seconds. An agent will be with you shortly.')
     .play('http://com.twilio.music.classical.s3.amazonaws.com/BusyStrings.mp3');
 };
-
-module.exports.waitResponseTwiml = waitResponseTwiml;
-module.exports.connectConferenceTwiml = connectConferenceTwiml;
