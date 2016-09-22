7229d3c3607b0a04ed0c3919c0ffd6b18bb8ac6b
cleancodeworld <killianlake2019@gmail.com>
2016-09-23 00:37:18 +0300

    redirect to my calls after set date

--- src/redux/modules/call/setSelectedDate.js
+++ src/redux/modules/call/setSelectedDate.js
@@ -4,9 +4,16 @@ export const SET_SELECTED_DATE = 'knexpert/call/setSelectedDate/LOAD';
 export const SET_SELECTED_DATE_SUCCESS = 'knexpert/call/setSelectedDate/SUCCESS';
 export const SET_SELECTED_DATE_FAIL = 'knexpert/call/setSelectedDate/FAIL';
 
-export function setSelectedDate(id, data) {
+export function _setSelectedDate(id, data) {
   return {
     types: [SET_SELECTED_DATE, SET_SELECTED_DATE_SUCCESS, SET_SELECTED_DATE_FAIL],
     promise: (client) => client.put(`api/v1/call/${id}/setdate`, { data })
   };
+}
+
+export function setSelectedDate(id, data) {
+  return dispatch => {
+    return dispatch(_setSelectedDate(id, data))
+      .then(()=> dispatch(push('/my-calls')))
+  }
 }
