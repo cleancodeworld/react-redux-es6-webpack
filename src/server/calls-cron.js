4ca4845a0f6e7a974d0e1228b2003a9bb68739fb
cleancodeworld <killianlake2019@gmail.com>
2016-09-23 16:33:52 +0300

    fix issue in trigger call

--- src/server/calls-cron.js
+++ src/server/calls-cron.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 export default new CronJob('*/1 * * * *', () => {
   async.waterfall([
     (callback)=> {
-      const path = `${config.apiUrl}/api/v1/call/date/${moment().set('second', 0).toISOString()}/${moment().set('second', 60).toISOString()}`;
+      const path = `${config.apiUrl}/api/v1/call/date/${moment().set('second', 0).toISOString()}/${moment().set('second', 59).toISOString()}`;
       // const path = `${config.apiUrl}/api/v1/call/date/2016-09-19T21:20:00Z/2016-09-19T21:40:00Z`
       console.log(path);
       return request
