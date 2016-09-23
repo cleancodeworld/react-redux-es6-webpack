424727bd57163df5b21c2381d675e7b3fda0e415
cleancodeworld <killianlake2019@gmail.com>
2016-09-23 16:42:03 +0300

    replace approve_by_expert with accepted_by_expert

--- src/components/CallListItem/types/Expert.js
+++ src/components/CallListItem/types/Expert.js
@@ -9,7 +9,7 @@ const Expert = ({call}) => {
           <h1 className="panel-title price"><b>[$16.67]</b></h1>
           <small>per minute</small>
           <Link className="btn btn-primary legitRipple mt-5 mb-5"
-                to={`/call/${call.get('id')}/approve_by_expert`}>Approve <i
+                to={`/call/${call.get('id')}/accepted_by_expert`}>Approve <i
             className="icon-arrow-right14 position-right"></i></Link>
           <div className="text-muted">
             <i className="icon-star-full2 text-size-base text-warning-300"></i>
